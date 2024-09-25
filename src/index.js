ae38269771bb00f1fb114958207f0fe22ae22b21
Samarth Ghatole <samarth.ghatole@iesoftek.com>
2024-09-25 18:38:57 +0530

    Add line numbers to diff

--- src/index.js
+++ src/index.js
@@ -134,7 +134,19 @@ async function run() {
 
     info(`Diff between: ${baseCommitHash}..${headCommitHash}`);
     const diff = shell
-        .exec(`git diff -W ${baseCommitHash}..${headCommitHash}`)
+        .exec(`git diff -W ${baseCommitHash}..${headCommitHash} | gawk '
+match($0,"^@@ -([0-9]+),([0-9]+) [+]([0-9]+),([0-9]+) @@",a){
+    left=a[1]
+    ll=length(a[2])
+    right=a[3]
+    rl=length(a[4])
+}
+/^(---|\\+\\+\\+|[^-+ ])/{ print;next }
+{ line=substr($0,2) }
+/^[-]/{ printf "-%"ll"s %"rl"s:%s\\n",left++,""     ,line;next }
+/^[+]/{ printf "+%"ll"s %"rl"s:%s\\n",""    ,right++,line;next }
+        { printf " %"ll"s %"rl"s:%s\\n",left++,right++,line }
+'`)
         .toString();
     // const { data: diff } = await octokit.repos.compareCommits({
     //     owner: context.repo.owner,
