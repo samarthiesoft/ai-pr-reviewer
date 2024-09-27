99d0b7bc467fde89367218490d9d5249cce8fc03
Samarth Ghatole <samarth.ghatole@iesoftek.com>
2024-09-27 20:12:05 +0530

    updated prompt

--- src/index.js
+++ src/index.js
@@ -242,6 +242,7 @@ Review the code and suggest changes that will make the code more maintanable, le
 While suggesting the changes kindly mention the from_line and to_line and the filename for the supplied code that you are suggesting the change against.
 For each suggestion mention the side. Can be LEFT or RIGHT. Use LEFT for deletions and RIGHT for additions.
 Strictly avoid repeating suggestions. Mention all the occurrences in one suggestion.
+All suggestions should strictly contain a text body.
 The lines that start with a + sign are the added lines
 The lines that start with a - sign are deleted lines
 The lines with a , are unmodified`,
