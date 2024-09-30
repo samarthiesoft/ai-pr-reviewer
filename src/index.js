264f390a6909f4fc2b64eb0deb94db5ed98b00bd
Samarth Ghatole <samarth.ghatole@iesoftek.com>
2024-09-30 21:09:29 +0530

    only print diff when debug

--- src/index.js
+++ src/index.js
@@ -114,7 +114,9 @@ async function run() {
         context.payload.pull_request.base.sha,
         headCommitHash
     );
-    info(`PR Diff: ${prDiff}\n\n`);
+    if (process.env.DEBUG == "true") {
+        info(`PR Diff: ${prDiff}\n\n`);
+    }
 
     info(`Gemini response - Summary:\n`);
     let summary = "";
